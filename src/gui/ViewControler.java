db164b4844ffe0589c219726cf15f20d1532bf73
MRLRSX <lucas.rocha-dev@outlook.com>
2022-09-26 19:46:05 -0300

    CONSTRAINTS

--- src/gui/ViewControler.java
+++ src/gui/ViewControler.java
@@ -1,13 +1,17 @@
 package gui;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
+import gui.utils.Constrains;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class ViewControler {
+public class ViewControler implements Initializable{
     
 	@FXML
 	private Button btSum;
@@ -29,5 +33,14 @@ public class ViewControler {
 		Double number1 = Double.parseDouble(txtNumber1.getText());
 		Double number2 = Double.parseDouble(txtNumber2.getText());
 		labelResult.setText(String.format("%.2f", number1 + number2));
+	}
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		Constrains.setTextFieldDouble(txtNumber1);
+		Constrains.setTextFieldDouble(txtNumber2);
+		Constrains.setTextFieldMaxLength(txtNumber1, 12);
+		Constrains.setTextFieldMaxLength(txtNumber2, 12);
 	}
 }
