bb1d9430ff383288f908040e55b16c46f45294dd
MRLRSX <lucas.rocha-dev@outlook.com>
2022-09-26 19:26:37 -0300

    SOMA

--- src/gui/ViewControler.java
+++ src/gui/ViewControler.java
@@ -1,17 +1,33 @@
 package gui;
 
-import gui.utils.Alerts;
+import java.util.Locale;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewControler {
     
 	@FXML
-	private Button btTest;
+	private Button btSum;
+	
 	
 	@FXML
-	public void onBtTestAction() {
-		Alerts.showAlert("Alert Title", "Alert Header", "HELLO", AlertType.INFORMATION);
+	private TextField txtNumber1 ;
+	
+	@FXML
+	private TextField txtNumber2 ;
+	
+	@FXML
+	private Label labelResult;
+	
+	
+	@FXML
+	public void onBtSumAction() {
+		Locale.setDefault(Locale.US);
+		Double number1 = Double.parseDouble(txtNumber1.getText());
+		Double number2 = Double.parseDouble(txtNumber2.getText());
+		labelResult.setText(String.format("%.2f", number1 + number2));
 	}
 }
